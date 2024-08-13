c7d6b92737dfb5174bbc5627230dce85cf4dda03
Miko Hadikusuma <uwmiko@hotmail.com>
2024-08-13 15:13:43 -0500

    Initial ASP.NET app project

---
+++ aspnetapp/Program.cs
@@ -0,0 +1,81 @@
+using System.Text.Json.Serialization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddHealthChecks();
+
+// builder.Services.ConfigureHttpJsonOptions(options =>
+// {
+//     options.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default);
+// });
+
+// Adds the Endpoint API Explorer service to the project
+builder.Services.AddEndpointsApiExplorer();
+// Adds Swagger generation service to the project
+// It will use API Explorer to generate the OpenAPI schema file
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.MapHealthChecks("/healthz");
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+// Enables swagger generation middleware
+app.UseSwagger();
+// Enables swagger UI
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+CancellationTokenSource cancellation = new();
+app.Lifetime.ApplicationStopping.Register( () =>
+{
+    cancellation.Cancel();
+});
+
+app.MapGet("/Environment", () =>
+{
+    return new EnvironmentInfo();
+});
+
+// This API demonstrates how to use task cancellation
+// to support graceful container shutdown via SIGTERM.
+// The method itself is an example and not useful.
+app.MapGet("/Delay/{value}", async (int value) =>
+{
+    try
+    {
+        await Task.Delay(value, cancellation.Token);
+    }
+    catch(TaskCanceledException)
+    {
+    }
+    
+    return new Operation(value);
+});
+
+app.Run();
+
+[JsonSerializable(typeof(EnvironmentInfo))]
+[JsonSerializable(typeof(Operation))]
+internal partial class AppJsonSerializerContext : JsonSerializerContext
+{
+}
+
+public record struct Operation(int Delay);
