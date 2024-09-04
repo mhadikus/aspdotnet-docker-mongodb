4673534fab51d1e2764a39590a0a0355d36478bf
Miko Hadikusuma <uwmiko@hotmail.com>
2024-09-04 17:43:58 -0500

    Refactor ProductsController

--- aspnetapp/Controllers/HomeController.cs
+++ aspnetapp/Controllers/HomeController.cs
@@ -1,7 +1,6 @@
 ﻿﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using aspnetapp.Models;
-using aspnetapp.Models.Mongo;
 
 namespace aspnetapp.Controllers
 {
@@ -11,34 +10,7 @@ namespace aspnetapp.Controllers
 
         public IActionResult Index()
         {
-            return View(nameof(Index), new ProductCollection());
-        }
-
-        public IActionResult Insert()
-        {
-            return View(nameof(Insert), new Product());
-        }
-
-        [HttpPost]
-        public IActionResult Insert(Product product)
-        {
-            var collection = new ProductCollection();
-            try
-            {
-                collection.Insert(product);
-                ViewBag.Message = $"Successfully added {product.Brand} {product.Model}";
-            }
-            catch (HttpRequestException exception)
-            {
-                ViewBag.Message = "ERROR: " + exception.Message;
-            }
-
-            return View(product);
-        }
-
-        public IActionResult Find()
-        {
-            return View(nameof(Find), new ProductCollection());
+            return RedirectToAction(nameof(ProductsController.Index), "Products");
         }
 
         public IActionResult Welcome()
---
+++ aspnetapp/Controllers/ProductsController.cs
@@ -0,0 +1,50 @@
+﻿﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using aspnetapp.Models;
+using aspnetapp.Models.Mongo;
+
+namespace aspnetapp.Controllers
+{
+    public class ProductsController(ILogger<ProductsController> logger) : Controller
+    {
+        private readonly ILogger<ProductsController> _logger = logger;
+
+        public IActionResult Index()
+        {
+            return View(new ProductCollection());
+        }
+
+        public IActionResult Insert()
+        {
+            return View(nameof(Insert), new Product());
+        }
+
+        [HttpPost]
+        public IActionResult Insert(Product product)
+        {
+            var collection = new ProductCollection();
+            try
+            {
+                collection.Insert(product);
+                ViewBag.Message = $"Successfully added {product.Brand} {product.Model}";
+            }
+            catch (HttpRequestException exception)
+            {
+                ViewBag.Message = "ERROR: " + exception.Message;
+            }
+
+            return View(product);
+        }
+
+        public IActionResult Find()
+        {
+            return View(nameof(Find), new ProductCollection());
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
